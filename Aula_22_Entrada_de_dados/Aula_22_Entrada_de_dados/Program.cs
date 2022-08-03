85db72d90007f2fd30bc1b76e805b6628b88c1d4
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-03 10:51:53 -0300

    22_Entrada_de_Dados
    Aula 22 - Entrada de dados

--- Aula_22_Entrada_de_dados/Aula_22_Entrada_de_dados/Program.cs
+++ Aula_22_Entrada_de_dados/Aula_22_Entrada_de_dados/Program.cs
@@ -6,7 +6,36 @@ namespace Aula_22_Entrada_de_dados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Notas:
+            {
+                /*
+                 Para entrada de dados usamos:
+                    Console.ReadLine();
+                    ◙ Lê da entrada padão etá a quebra de linha
+                    ◙ Retorna os dados lidos na forma de String.
+                 */
+            }
+            string frase = Console.ReadLine(); //Entrada de dados
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+
+           
+            string[] v = Console.ReadLine().Split('-');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
         }
     }
 }
