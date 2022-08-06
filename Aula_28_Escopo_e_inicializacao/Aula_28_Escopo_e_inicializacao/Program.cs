0c247559d06b15de6ca7ca2e4be829bf5a4eddce
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-06 10:44:22 -0300

    Aula_28_Escopo
    Aplicando as correções

--- Aula_28_Escopo_e_inicializacao/Aula_28_Escopo_e_inicializacao/Program.cs
+++ Aula_28_Escopo_e_inicializacao/Aula_28_Escopo_e_inicializacao/Program.cs
@@ -6,7 +6,18 @@ namespace Aula_28_Escopo_e_inicializacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double preco = double.Parse(Console.ReadLine()); //Variavel preco pertence ao escopo principal
+            double desconto = 0;//Correção
+
+            if (preco > 100)
+            {
+                //double desconto = preco * 0,1; //anterior, errado, declarando direto no escopo do if
+                desconto = preco * 0.1;
+            }
+            Console.WriteLine(desconto);//Erro - A variavel desconto pertence ao escopo menor, e está sendo 
+            //requisistada em um escopo maior!
+            //Do escopo maior para o menor é possivel, do menor para o maior não é possivel!
         }
+
     }
 }
