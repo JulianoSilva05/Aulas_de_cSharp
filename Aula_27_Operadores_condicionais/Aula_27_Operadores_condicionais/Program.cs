119c8d5bfacc535912d58257de6d358f2bd9288a
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-06 10:28:54 -0300

    Aula_27_Estrutura_Condicional
    Conteudo sobre estrutura condicional if / else

---
+++ Aula_27_Operadores_condicionais/Aula_27_Operadores_condicionais/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_27_Operadores_condicionais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a hora?: ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            
+                Console.WriteLine("Bom Dia!");
+            
+
+            else if (hora < 18)
+            {
+                Console.WriteLine("Boa Tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa Noite!");
+            }
+        }
+    }
+}
