dfb99f51310e6d23fa821f4998146ac3a9521f1b
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-09 19:45:52 -0300

    ercicios_propostos_2_condicao
    Estrutura While

---
+++ Exercicio_propostos_3_while/Exercicio_propostos_3_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercicio_propostos_3_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva um programa que repita a leitura de uma senha até que ela seja válida. " +
+                "Para cada leitura de senha incorreta informada, escrever a mensagem " +
+                " Senha Invalida.Quando a senha for informada corretamente deve ser " +
+                "impressa a mensagem Acesso Permitido e o algoritmo encerrado.Considere " +
+                "que a senha correta é o valor 2002");
+            double senha = double.Parse(Console.ReadLine());
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
---
+++ Exercicio_propostos_3_while_2/Exercicio_propostos_3_while_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio_propostos_3_while_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
