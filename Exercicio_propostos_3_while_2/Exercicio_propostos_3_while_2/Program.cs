7449e0906bfcce8d4946a416b8745c2538a96cd5
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-15 17:36:27 -0300

    Exercicio_propostos_3_while_3
    ercicios_propostos_3_condicao

--- Exercicio_propostos_3_while_2/Exercicio_propostos_3_while_2/Program.cs
+++ Exercicio_propostos_3_while_2/Exercicio_propostos_3_while_2/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Exercicio_propostos_3_while_2
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+
+            while (x != 0 && y != 0)
+            {
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else
+                {
+                    Console.WriteLine("quarto");
+                }
+                valores = Console.ReadLine().Split(' ');
+                x = int.Parse(valores[0]);
+                y = int.Parse(valores[1]);
+            }
         }
     }
 }
---
+++ Exercicio_propostos_3_while_3/Exercicio_propostos_3_while_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio_propostos_3_while_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int alcool = 0;
+            int gasolina = 0;
+            int diesel = 0;
+
+            int tipo = int.Parse(Console.ReadLine());
+
+            while (tipo != 4)
+            {
+                if (tipo == 1)
+                {
+                    alcool = alcool + 1;
+                }
+                else if (tipo == 2)
+                {
+                    gasolina = gasolina + 1;
+                }
+                else if (tipo == 3)
+                {
+                    diesel = diesel + 1;
+                }
+
+                tipo = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("MUITO OBRIGADO");
+            Console.WriteLine("Alcool: " + alcool);
+            Console.WriteLine("Gasolina: " + gasolina);
+            Console.WriteLine("Diesel: " + diesel);
+        }
+    }
+}
