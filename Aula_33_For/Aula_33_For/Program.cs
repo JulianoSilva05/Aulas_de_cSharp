43a18beebd8501fd7d7d68a29a5736144571cedb
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-17 21:30:52 -0300

    Aula_34_For
    Repetição com FOR

---
+++ Aula_33_For/Aula_33_For/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula_33_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar?");
+            int N = int.Parse(Console.ReadLine());
+            int soma = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("Valor #{0}: ",i);
+                int valor = int.Parse(Console.ReadLine());
+                soma = soma + valor;
+            }
+            Console.WriteLine("Soma = " + soma);
+
+        }
+    }
+}
