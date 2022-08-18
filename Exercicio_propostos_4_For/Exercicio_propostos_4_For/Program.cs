cad45a9ae1292e8a0d234b9edbf25ea80d9ecc5d
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2022-08-17 21:53:02 -0300

    Exercicio_propostos_4_For
    Exercicio propostos _FOR

---
+++ Exercicio_propostos_4_For/Exercicio_propostos_4_For/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercicio_propostos_4_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com um numero");
+            int valor = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= valor; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
